1d42b11e6856da69b64b70e94679e18b3d18c64b
baipiyachat-sudo <baipiyachat@gmail.com>
2026-04-22 11:55:25 +0700

    commit

---
+++ fff.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+// รับค่าชื่อจาก URL เช่น hello.php?name=สมชาย
+$name = isset($_GET['name']) ? htmlspecialchars($_GET['name']) : "ผู้เยี่ยมชม";
+echo "สวัสดี " . $name;
+?>
