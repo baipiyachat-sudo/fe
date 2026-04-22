ff4e637102f1b10a74b2d8f688b702290b9b0d44
baipiyachat-sudo <baipiyachat@gmail.com>
2026-04-22 12:06:16 +0700

    o

--- fff.php
+++ fff.php
@@ -2,5 +2,5 @@
 header("Content-Type: text/html; charset=utf-8");
 // รับค่าชื่อจาก URL เช่น hello.php?name=สมชาย
 $name = isset($_GET['name']) ? htmlspecialchars($_GET['name']) : "ผู้เยี่ยมชม";
-echo "สวัสดี " . $name;
+echo "สวัสดี " . $name; 88888888888888888
 ?>
